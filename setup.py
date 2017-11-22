72005b5165ade17f8119294be4c0063645e718e4
decalogue <1044908508@qq.com>
2017-11-22 11:34:48 +0800

    pytest

--- setup.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import setuptools
-
-setuptools.setup(setup_requires=['pbr'], pbr=True)
---
+++ tests/test_sample.py
@@ -0,0 +1,5 @@
+def func(x):  
+    return x+1  
+  
+def test_func():  
+    assert func(3) == 4
