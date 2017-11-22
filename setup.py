785dd48b7694ea4e51aa44d7134c2a8942a0a8a8
decalogue <1044908508@qq.com>
2017-11-22 13:48:48 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
-	
+from setuptools import setup, find_packages
+
 setup(
     name="ai",
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
     author="Decalogue",
     version="0.0.1",
     author_email="1044908508@qq.com",
     description="Ai lib.",
     license="MIT",
     url="https://github.com/Decalogue/ai",
-    packages=["ai"],
+    packages=find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
         "Natural Language :: English",
-		"Natural Language :: Chinese (Simplified)",
+        "Natural Language :: Chinese (Simplified)",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX :: Linux",
