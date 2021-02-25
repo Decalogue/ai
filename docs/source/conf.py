391cf29b62974148d3ecc5015bc793cf63be25de
Decalogue <1044908508@qq.com>
2021-02-25 12:58:59 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,9 +21,6 @@ project = 'ai'
 copyright = '2021, Decalogue'
 author = 'Decalogue'
 
-# The full version, including alpha/beta/rc tags
-release = 'MIT'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -39,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. ai documentation master file, created by
-   sphinx-quickstart on Thu Feb 25 12:31:58 2021.
+   sphinx-quickstart on Thu Feb 25 12:51:13 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
