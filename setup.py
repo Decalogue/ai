2fef4aecc77bed70368ad67de01e216090a98780
Decalogue <decalogue80@gmail.com>
2025-10-30 13:31:04 +0800

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ if sys.argv[-1] == 'tag':
     os.system("git push --all origin")
     sys.exit()
 elif sys.argv[-1] == 'publish':
-    os.system("python setup.py sdist")
+    os.system("rm -rf dist/ build/ *.egg-info")  # 先清理旧文件
+    os.system("python setup.py sdist bdist_wheel")  # 构建两种格式
     os.system("twine upload dist/*")
     sys.exit()
 elif sys.argv[-1] == 'test':
@@ -32,13 +33,17 @@ elif sys.argv[-1] == 'test':
 
 setup(
     name="ai",
-    author="Decalogue",
     version=version,
+    author="Decalogue",
     author_email="1044908508@qq.com",
     description="Ai lib.",
+    long_description=open('README.rst').read() if os.path.exists('README.rst') else "Ai lib.",
+    long_description_content_type="text/x-rst",
     license="MIT",
     url="https://github.com/Decalogue/ai",
     packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
@@ -53,8 +58,8 @@ setup(
         "Operating System :: Other OS",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10"
-        "Programming Language :: Python :: 3.11"
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12"
     ]
 )
